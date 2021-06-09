2cb32f0c4e0204b08d33d062d1e141d04c4f8d73
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-09 14:38:35 -0500

    notes apiRoutes added

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+const { notes } = require("../../db/db");
+
+router.get("/notes", (req, res) => {
+  let results = notes;
+
+  res.json(results);
+});
+
 module.exports = router;
