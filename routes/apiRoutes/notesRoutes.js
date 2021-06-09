36b2e0d9d0d56f55b5c354da414d9b0ea85c4c20
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-09 16:27:35 -0500

    delete route for notes fixed

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
 
 const { createNewNote } = require("../../lib/notes");
 const { notes } = require("../../db/db");
@@ -15,6 +17,26 @@ router.post("/notes", (req, res) => {
 
   const note = createNewNote(req.body, notes);
   res.json(note);
+});
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+      break;
+    }
+  }
+}
+
+router.delete("/notes/:id", (req, res) => {
+  deleteNote(req.params.id, notes);
+  res.json(true);
 });
 
 module.exports = router;
